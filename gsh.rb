4f40dad0f3a83897a59c154eca4a25d02c322624
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-03-18 11:31:16 -0300

    [FEAT] Add first version of homebrew formula

---
+++ gsh.rb
@@ -0,0 +1,16 @@
+class GSH < Formula
+    desc "A command line tool to use GSH"
+    homepage "https://github.com/globocom/gsh"
+    url "https://github.com/globocom/gsh/releases/download/v0.1/gsh-cli-darwin-amd64"
+    sha256 "f408bc853faecf6dd5d48ab54606dc0c04c054972d9d11312a64bc21712f5607"
+    version "0.1.0"
+  
+    depends_on "openssh"
+  
+    bottle :unneeded
+  
+    def install
+      prefix.install "gsh-cli-darwin-amd64", "gsh"
+      bin.install "gsh"
+    end
+  end
