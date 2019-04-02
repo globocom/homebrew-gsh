254217051ea379e0d34af0706ac23e7396b4d187
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-04-02 10:54:21 -0300

    [FEAT] Using brew to build GSH CLI

--- gsh.rb
+++ gsh.rb
@@ -2,13 +2,18 @@
 class Gsh < Formula
   desc "A command line tool to use GSH"
   homepage "https://github.com/globocom/gsh"
-  url "https://github.com/globocom/gsh/releases/download/v0.1.2/gsh-darwin-amd64.tar.gz"
+  url "https://github.com/globocom/gsh/archive/v0.1.2.tar.gz"
   version "0.1.2"
-  sha256 "f94e1d3d1a05e83a659ac2954011db223f33389b3afa4f7c8ec9cce8c220aa1b"
+  sha256 "76ae796318cca8d5a909d8a4b5fc6ccef523b3c791f1ffab70a6e5bbaf43d6c8"
   
   depends_on "openssh"
+  depends_on "go"
+  depends_on "dep"
 
   def install
+    ENV["GOPATH"] = buildpath
+    system "dep", "ensure"
+    system "go", "build", "-o", "gsh", "cli/main.go"
     bin.install "gsh"
   end
 end
