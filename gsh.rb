d6e7dd1ae6844c0c68a498d042fe6937b61c55ce
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-04-02 11:07:12 -0300

    [FEAT] Using git tag and revision at formula

--- gsh.rb
+++ gsh.rb
@@ -2,9 +2,9 @@
 class Gsh < Formula
   desc "A command line tool to use GSH"
   homepage "https://github.com/globocom/gsh"
-  url "https://github.com/globocom/gsh/archive/v0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "76ae796318cca8d5a909d8a4b5fc6ccef523b3c791f1ffab70a6e5bbaf43d6c8"
+  url "https://github.com/globocom/gsh.git"
+      :tag      => "v0.1.2",
+      :revision => "8b0545ec92113afdb8ad130aba913269acec9aab"
   
   depends_on "openssh"
   depends_on "go"
@@ -12,8 +12,12 @@ class Gsh < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    system "dep", "ensure"
-    system "go", "build", "-o", "gsh", "cli/main.go"
-    bin.install "gsh"
+    (buildpath/"src/github.com/globocom/gsh").install buildpath.children
+    cd "src/github.com/globocom/gsh" do
+      system "dep", "ensure"
+      system "go", "build", "-o", "gsh", "./cli/main.go"
+      # prefix.install_metafiles
+      bin.install "gsh"
+    end
   end
 end
